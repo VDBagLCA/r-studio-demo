3fbed61c56bf2f568ecd450d3d0551aa725e2e88
VDBagLCA <vincent.desaulniersbrousseau@agresearch.co.nz>
2025-02-18 10:04:15 +1300

    asdf

---
+++ source_code.R
@@ -0,0 +1,9 @@
+# test file for test
+
+Royal Society Te Apārangi has opened the 
+January 2025 Call for Catalyst: Seeding and Catalyst: Leaders.  Noting this is opening later than usual but the deadline dates (17 April) remain the same.
+Catalyst: Seeding facilitates new small to medium pre-research strategic partnerships with international collaborators that cannot be supported through other means, and with a view to developing full collaborations that could be supported through other research funding mechanisms, including those administered by the Ministry.
+Catalyst: Leaders supports incoming and outgoing targeted international fellowships for exceptional individuals that cannot be supported through other means. 
+The current Seeding fund available is: 
+  
+  
